3cc0ae53a11e1f96be9285322dbd286de6c47678
Gurpreet Singh <singhg469@gmail.com>
2020-07-06 12:31:34 +0530

    Updates

---
+++ precache-manifest.963ede6a516c44429b74fc9e56ce1dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52233b16a81f547c000659dcca761386",
+    "url": "/honey/index.html"
+  },
+  {
+    "revision": "8b2a408abb7cf4b59dce",
+    "url": "/honey/static/css/main.70b6f950.chunk.css"
+  },
+  {
+    "revision": "eb689abf9c94d2610581",
+    "url": "/honey/static/js/2.742abfbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/honey/static/js/2.742abfbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2a408abb7cf4b59dce",
+    "url": "/honey/static/js/main.38364ea0.chunk.js"
+  },
+  {
+    "revision": "fcb6dcf2352ea6366a7c",
+    "url": "/honey/static/js/runtime-main.6a13eb47.js"
+  }
+]);
